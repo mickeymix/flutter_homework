004ac9944579d1c69b8b7695fa437af902f9816c
mickamix <mickey_mix@live.com>
2018-11-25 13:33:33 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/b/pongphon/flutterhomework/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.b.pongphon.flutterhomework
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
